380bc459cadbe97379a8af7229365f4e536c188a
Frediano Ziglio <freddy77@gmail.com>
2010-12-31 18:43:58 +0100

    files for RLog build

---
+++ rlog.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+NAME=rlog-1.4
+OUT=$PWD/out
+rm -rf $NAME
+tar zxvf $NAME.tar.gz
+patch -p0 < $NAME.diff
+cd $NAME
+./configure --prefix=$OUT --host=i586-mingw32msvc
+make
+make install
+cd ..
+rm -rf $NAME
+echo RLog ok

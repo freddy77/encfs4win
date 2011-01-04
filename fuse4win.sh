fc02f6c8e9fad0370f69752966790c98cb36c2c0
Frediano Ziglio <freddy77@gmail.com>
2011-01-04 11:19:41 +0100

    updated

--- fuse4win.sh
+++ fuse4win.sh
@@ -1,29 +1,12 @@
 #!/bin/bash
 set -e
-NAME=fuse4win-070d3a2e43de
-
-# check we are not working
-if test -d $NAME; then
-	echo "Please delete directory manually... work in progress" >&2
-	exit 1
-fi
-
 OUT=$PWD/out
-rm -rf $NAME
-tar jxvf $NAME.tar.bz2
-cp dokan.h $NAME/src/dokan.h
-cd $NAME
-rm -rf patches
-tar zxvf ../fuse_patches.tar.gz
-quilt push -a
+cd fuse4win-070d3a2e43de
+rm -rf build
 mkdir build
 cd build
-CC=i586-mingw32msvc-gcc CXX=i586-mingw32msvc-g++ cmake ..
+cmake -DCMAKE_TOOLCHAIN_FILE=../cross.cmake -DCMAKE_INSTALL_PREFIX=$OUT .. 
 make
-i586-mingw32msvc-ranlib libfuse_static.a
-cp libfuse_static.a $OUT/lib/libfuse.a
-cd ../include
-cp *.h $OUT/include
+make install
 cd ../..
-rm -rf $NAME
 echo fuse4win ok

a73cf0684f89382fc4ff4e01e002bd9136bf4f99
Frediano Ziglio <freddy77@gmail.com>
2011-01-27 13:50:11 +0100

    add needed win32 libraries

--- encfs.sh
+++ encfs.sh
@@ -7,7 +7,6 @@ autoreconf
 export lt_cv_deplibs_check_method='pass_all'
 LDFLAGS=-L$OUT/lib CPPFLAGS=-I$OUT/include ./configure --prefix=$OUT --with-boost=$OUT --without-libiconv-prefix --without-libintl-prefix --host=i586-mingw32msvc
 cd encfs
-perl -i.orig -pe 's/-lboost_serialization -lboost_filesystem/-lboost_serialization -lboost_filesystem -lboost_system -lws2_32 -luser32 -lgdi32/' Makefile
 make encfs.exe
 cp .libs/encfs.exe $OUT/bin
 cd ../..

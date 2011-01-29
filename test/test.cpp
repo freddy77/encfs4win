d7c6134ae9b1c3d6e26baeda24547973417e3cc2
Frediano Ziglio <freddy77@gmail.com>
2011-01-29 11:12:25 +0100

    more tests and style

--- test/test.cpp
+++ test/test.cpp
@@ -25,6 +25,7 @@ check(int line, bool test, const char *chk, const char *fmt, ...)
 #define CHECK(test, arg...) do { SetLastError(0); check(__LINE__, test, #test, arg); } while(0)
 
 static const char fn[] = "testfile.txt";
+static const char fn2[] = "testfile2.txt";
 
 int main()
 {
@@ -60,37 +61,55 @@ int main()
 	h1 = CreateFile(fn, GENERIC_READ, 0, NULL, OPEN_EXISTING, 0, NULL);
 	CHECK(h1 != INVALID_HANDLE_VALUE, NULL);
 	CHECK(LockFile(h1, 0, 0, 10, 0), "lock failed");
-	CHECK(!LockFile(h1, 0, 0, 10, 0), "lock succeeded");	// double lock must fail
+	CHECK(!LockFile(h1, 0, 0, 10, 0), "double lock succeeded");
 	CHECK(UnlockFile(h1, 0, 0, 10, 0), "unlock failed");
 	CHECK(!UnlockFile(h1, 0, 0, 20, 0), "unlock succeeded");
-	// TODO check overlapping locks
+
+	// check overlapping locks
 	CHECK(LockFile(h1, 10, 0, 10, 0), "lock failed");
 	CHECK(LockFile(h1, 20, 0, 10, 0), "lock failed");
 	CHECK(LockFile(h1, 40, 0, 10, 0), "lock failed");
 
 	// (un)lock that overlap (inside, middle, side, two contiguos)
-	CHECK(!LockFile(h1, 15, 0, 3,  0), "lock succeeded");	// inside
-	CHECK(!LockFile(h1, 10, 0, 5,  0), "lock succeeded");	// side
-	CHECK(!LockFile(h1, 15, 0, 5,  0), "lock succeeded");	// side
-	CHECK(!LockFile(h1, 10, 0, 20, 0), "lock succeeded");	// two contiguos
-	CHECK(!LockFile(h1, 30, 0, 20, 0), "lock succeeded");	// one full + free
+	CHECK(!LockFile(h1, 15, 0, 3,  0), "lock succeeded inside");
+	CHECK(!LockFile(h1, 10, 0, 5,  0), "lock succeeded on left side");
+	CHECK(!LockFile(h1, 15, 0, 5,  0), "lock succeeded on right side");
+	CHECK(!LockFile(h1, 10, 0, 20, 0), "lock succeeded of 2 contiguos");
+	CHECK(!LockFile(h1, 30, 0, 20, 0), "lock succeeded 1+free piece");
 
-	CHECK(!UnlockFile(h1, 15, 0, 3,  0), "unlock succeeded");	// inside
-	CHECK(!UnlockFile(h1, 10, 0, 5,  0), "unlock succeeded");	// side
-	CHECK(!UnlockFile(h1, 15, 0, 5,  0), "unlock succeeded");	// side
-	CHECK(!UnlockFile(h1, 10, 0, 20, 0), "unlock succeeded");	// two contiguos
-	CHECK(!UnlockFile(h1, 30, 0, 20, 0), "unlock succeeded");	// one full + free
+	CHECK(!UnlockFile(h1, 15, 0, 3,  0), "unlock succeeded inside");
+	CHECK(!UnlockFile(h1, 10, 0, 5,  0), "unlock succeeded on left side");
+	CHECK(!UnlockFile(h1, 15, 0, 5,  0), "unlock succeeded on right side");
+	CHECK(!UnlockFile(h1, 10, 0, 20, 0), "unlock succeeded of 2 contiguos");
+	CHECK(!UnlockFile(h1, 30, 0, 20, 0), "unlock succeeded 1+free piece");
 
 	CHECK(UnlockFile(h1, 20, 0, 10, 0), "unlock failed");
 	CHECK(UnlockFile(h1, 10, 0, 10, 0), "unlock failed");
 	CHECK(UnlockFile(h1, 40, 0, 10, 0), "unlock failed");
 	CloseHandle(h1);
 
+	printf("check a bug with CREATE_ALWAYS\n");
+	h1 = CreateFile(fn, FILE_GENERIC_WRITE|FILE_READ_ATTRIBUTES, FILE_SHARE_READ, NULL, CREATE_ALWAYS, 0, NULL);
+	CHECK(h1 != INVALID_HANDLE_VALUE, "create failed");
+	CloseHandle(h1);
+
+	printf("rename to an open file\n");
+	h1 = CreateFile(fn2, SYNCHRONIZE|FILE_WRITE_DATA, FILE_SHARE_WRITE|FILE_SHARE_READ, NULL, CREATE_ALWAYS, 0, NULL);
+	CHECK(h1 != INVALID_HANDLE_VALUE, "create failed");
+	CHECK(!MoveFile(fn, fn2), "move succeeded");
+	CloseHandle(h1);
+	DeleteFile(fn2);
+
 	printf("deleting while open\n");
 	h1 = CreateFile(fn, FILE_READ_ATTRIBUTES, FILE_SHARE_DELETE, NULL, OPEN_EXISTING, 0, NULL);
 	CHECK(h1 != INVALID_HANDLE_VALUE, NULL);
 	CHECK(DeleteFile(fn), "failed deleting file");
+	WIN32_FIND_DATA wfd;
+	h2 = FindFirstFile(fn, &wfd);
+	CHECK(h2 != INVALID_HANDLE_VALUE, NULL);
+	FindClose(h2);
 	CloseHandle(h1);
+	CHECK(FindFirstFile(fn, &wfd) == INVALID_HANDLE_VALUE, NULL);
 
 	printf("all done!\n");
 

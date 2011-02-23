1022456208fbc7533968b27d913ed67ea961a971
Frediano Ziglio <freddy77@gmail.com>
2011-02-23 14:10:20 +0100

    provide libfuse.a too

--- fuse4win.sh
+++ fuse4win.sh
@@ -5,8 +5,13 @@ cd fuse4win-070d3a2e43de
 rm -rf build
 mkdir build
 cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=../cross.cmake -DCMAKE_INSTALL_PREFIX=$OUT .. 
+cmake -DCMAKE_TOOLCHAIN_FILE=../cross.cmake -DCMAKE_INSTALL_PREFIX=$OUT ..
+#LDFLAGS=-pg CFLAGS="-DSTATISTICS -pg -fprofile" CXXFLAGS="-DSTATISTICS -pg -fprofile" cmake -DCMAKE_TOOLCHAIN_FILE=../cross.cmake -DCMAKE_INSTALL_PREFIX=$OUT ..
 make
 make install
 cd ../..
+cd out/lib
+test -r libfuse_static.a
+rm -f libfuse.a
+ln -s libfuse_static.a libfuse.a 
 echo fuse4win ok

df14a37cec3ae0a21b91a60e3cbff1ea3f02edb1
Frediano Ziglio <freddy77@gmail.com>
2011-02-25 09:42:03 +0100

    compile encfsctl too

--- encfs.sh
+++ encfs.sh
@@ -2,13 +2,17 @@
 set -e
 OUT=$PWD/out
 cd encfs
-make distclean || true
-autoreconf
-export lt_cv_deplibs_check_method='pass_all'
-LDFLAGS=-L$OUT/lib CPPFLAGS=-I$OUT/include ./configure --prefix=$OUT --with-boost=$OUT --without-libiconv-prefix --without-libintl-prefix --host=i586-mingw32msvc
+if true; then
+	make distclean || true
+	autoreconf
+	export lt_cv_deplibs_check_method='pass_all'
+	#LDFLAGS="-L$OUT/lib -pg" CXXFLAGS="-pg -fprofile" CFLAGS="-pg -fprofile" CPPFLAGS="-I$OUT/include" ./configure --prefix=$OUT --with-boost=$OUT --without-libiconv-prefix --without-libintl-prefix --host=i586-mingw32msvc
+	LDFLAGS=-L$OUT/lib CPPFLAGS=-I$OUT/include ./configure --prefix=$OUT --with-boost=$OUT --without-libiconv-prefix --without-libintl-prefix --host=i586-mingw32msvc
+fi
 cd encfs
-make encfs.exe
-cp .libs/encfs.exe $OUT/bin
+rm -f encfs.exe encfsctl.exe
+make encfs.exe encfsctl.exe
+cp .libs/encfs.exe .libs/encfsctl.exe $OUT/bin
 cd ../..
 echo encfs ok
 

e964aa6d544e0b3b486146b8083f509244d16599
Frediano Ziglio <freddy77@gmail.com>
2011-05-02 13:44:02 +0200

    added initial resources

---
+++ encfs/encfs/win/resource.h
@@ -0,0 +1,13 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDR_MANIFEST                            1
+#define IDD_DLG_DIALOG                          102
+#define IDD_ABOUTBOX                            103
+#define IDC_MYICON                              105
+#define IDI_MAIN                                108
+#define IDOK1                                   1000
+#define IDC_DIR                                 1004
+#define IDC_CMBDRIVE                            1005
+#define IDC_BTNDIR                              1007

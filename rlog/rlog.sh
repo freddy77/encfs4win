adfb3569a8c6339317cc2f49ff83b6076c554f4f
Frediano Ziglio <freddy77@gmail.com>
2012-08-30 21:44:24 +0100

    add patch for Microsoft Visual C++ detect proper cross compiler to use

--- rlog/rlog.sh
+++ rlog/rlog.sh
@@ -5,8 +5,20 @@ OUT=$PWD/../out
 rm -rf $NAME
 tar zxvf $NAME.tar.gz
 patch -p0 < $NAME.diff
+patch -p0 < rlog-1.4-win.diff
 cd $NAME
-./configure --prefix=$OUT --host=i586-mingw32msvc
+HOST=unknown
+for h in i386-mingw32 i586-mingw32msvc i686-w64-mingw32; do
+	if $h-gcc --help &> /dev/null; then
+		HOST=$h
+		break
+	fi
+done
+if [ $HOST = unknown ]; then
+	echo Host not detected >&2
+	exit 1
+fi
+./configure --prefix=$OUT --host=$HOST
 make
 make install
 cd ..
